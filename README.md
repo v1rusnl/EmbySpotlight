4a3129afd83cf51480e7e1f5ef9214c0e67b3b49
v1rusnl <18641204+v1rusnl@users.noreply.github.com>
2025-10-08 08:10:25 +0200

    Modifiy usage instructions

--- README.md
+++ README.md
@@ -29,11 +29,11 @@ A Spotlight banner for Emby Media Server
  default = 8000ms (8s)
  ```
 
- c) backgroundColor variable (line 30) is for the gradient/vignette color at the inside edges of the spotlight and can be any value, e.g.: 
+ c) backgroundColor variable (line 30) is for the gradient/vignette color at the inside edges of the spotlight and can be any supported value of: 
 ```
 HEX: "#0000000" -> Emby Themes: Dark = #1e1e1e; Black = #000000; Light = #ffffff; Finimalism inspired = #090214; for other gradient themes like AppleTV or Blue Radiance take e.g. Windows Color Picker (WIN+SHIFT+C) and choose a color on the screen that makes you happy
 ```
- d) highlightColor variable (line 31) is for the border around the spotlight on hover and can be any valid value, e.g.: 
+ d) highlightColor variable (line 31) is for the border around the spotlight on hover and can be any supported value of: 
  ```
  HEX: "#0000000"
  rgb: "rgb(20 170 223)"
--- Spotlight.js
+++ Spotlight.js
@@ -7,9 +7,9 @@
  * 2. Change the following values to your needs
  *    a) limit variable (line 28) is for the amount of items from 50 latest the plugin shows in Spotlight in random order -> default = 10 items
  *    b) autoplayInterval variable (line 29) sets the amount of time how long an item is presented by Spotlight -> default = 8000ms (8s)
- *    c) backgroundColor variable (line 30) is for the gradient/vignette color at the inside edges of the spotlight and can be any value, e.g.: 
+ *    c) backgroundColor variable (line 30) is for the gradient/vignette color at the inside edges of the spotlight and can be any supported value of: 
  *       HEX: "#0000000" -> Emby Themes: Dark = #1e1e1e; Black = #000000; Light = #ffffff; Finimalism Inspired = #090214; for other gradient themes like AppleTV or Blue Radiance take e.g. Windows Color Picker (WIN+SHIFT+C) and choose a color on the screen that makes you happy
- *    d) highlightColor variable (line 31) is for the border around the spotlight on hover and can be any valid value, e.g.: 
+ *    d) highlightColor variable (line 31) is for the border around the spotlight on hover and can be any supported value of: 
  *       HEX: "#0000000"
  *       rgb: "rgb(20 170 223)"
  *       rgba: "rgba(20 170 223, 0.2)"
